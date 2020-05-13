bc68e3be3ebb2ce116bbd85aaaaf6fdfd6ebe176
JulioAvalos <julioavalos2@msn.com>
2020-05-13 01:44:51 -0600

    Adding recommended hikari config

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbeerservice.bootstrap;
 
 import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,8 @@ import java.math.BigDecimal;
 /**
  * Created by jt on 2019-05-17.
  */
-//@Component
+@Component
+@RequiredArgsConstructor
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -19,44 +21,37 @@ public class BeerLoader implements CommandLineRunner {
 
     private final BeerRepository beerRepository;
 
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
-
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObjects();
+        if (beerRepository.count() == 0) loadBeerObjects();
     }
 
     private void loadBeerObjects() {
-        if(beerRepository.count() == 0){
+        beerRepository.save(Beer.builder()
+                .beerName("Mango Bobs")
+                .beerStyle("IPA")
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_1_UPC)
+                .price(new BigDecimal("12.95"))
+                .build());
 
-            beerRepository.save(Beer.builder()
-                    .beerName("Mango Bobs")
-                    .beerStyle("IPA")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95"))
-                    .build());
+        beerRepository.save(Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle("PALE_ALE")
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_2_UPC)
+                .price(new BigDecimal("11.95"))
+                .build());
 
-            beerRepository.save(Beer.builder()
-                    .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
-
-            beerRepository.save(Beer.builder()
-                    .beerName("No Hammers On The Bar")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
-        }
+        beerRepository.save(Beer.builder()
+                .beerName("No Hammers On The Bar")
+                .beerStyle("PALE_ALE")
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_3_UPC)
+                .price(new BigDecimal("11.95"))
+                .build());
     }
 }
