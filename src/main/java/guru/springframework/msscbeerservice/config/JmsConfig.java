6f0e1818c10ce3316b6b8d5b9f5864a1db506e69
JulioAvalos <julioavalos2@msn.com>
2021-03-02 00:12:05 -0600

    syncronizing files

--- src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -12,8 +12,9 @@ public class JmsConfig {
 
     public final static String BREWING_REQUEST_QUEUE = "brewing-request";
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
 
-    @Bean
+    @Bean //serialize message content to json using TextMessage
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper){
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
