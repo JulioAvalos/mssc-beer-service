005949a1f5c9b11b2b65ecbe6866c46f53d69897
Julio Avalos <julioavalos2@msn.com>
2021-03-06 23:08:56 -0600

    Implement Validate Order Listener closes #13

--- src/test/java/guru/springframework/msscbeerservice/service/inventory/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/guru/springframework/msscbeerservice/service/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -1,16 +1,18 @@
 package guru.springframework.msscbeerservice.service.inventory;
 
-import guru.springframework.msscbeerservice.bootstrap.BeerLoader;
+
 import guru.springframework.msscbeerservice.services.inventory.BeerInventoryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Disabled
 @SpringBootTest
 public class BeerInventoryServiceRestTemplateImplTest {
 
+    @Autowired
     BeerInventoryService beerInventoryService;
 
     @BeforeEach
